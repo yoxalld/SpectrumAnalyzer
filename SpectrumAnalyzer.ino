7f47aa94722089a276d9103f3226508b793ec436
Dustin Yoxall <dustin@cakeandeatitdesigns.com>
2017-07-06 18:30:39 -0400

    Basic working code for 7 strips

--- SpectrumAnalyzer.ino
+++ SpectrumAnalyzer.ino
@@ -1,8 +1,9 @@
 #include <FastLED.h>
 #define NUM_LEDS 60
 // NUM_STRPS more than 7 won't work since we only have 7 frequency channels
-#define NUM_STRPS 2
+#define NUM_STRPS 7
 #define DATA_PIN 9
+#define BRIGHT 45
 
 CRGB leds[NUM_LEDS * NUM_STRPS];
 
@@ -51,7 +52,8 @@ void setup() {
 
 void loop() {
   Read_Frequencies();
-  Graph_Frequencies();
+  //Graph_Frequencies();
+  Graph_Frequencies_Adjusted();
   FastLED.show();
   delay(60);
 
@@ -72,29 +74,82 @@ void Graph_Frequencies(){
     // We set the base and peak of the string of leds we want to work with
     base = i * NUM_LEDS;
     peak = (i * NUM_LEDS)-1 + NUM_LEDS;
-    Serial.print("Start");
-    Serial.println(base);
-    Serial.println(peak);
     // We use the higher of the two stereo frequency values
     if(Frequencies_Two[i] > Frequencies_One[i]){
       // The number of leds we want to be light up
       litPix = Frequencies_Two[i] / divisor;
       // We loop through and light up the leds for the strip we are on
       for( p = base; p <= base + litPix; p ++ ) {
-        leds[p]= CHSV( Frequencies_Two[i]/4, 255, 25);
+        leds[p]= CHSV( Frequencies_Two[i]/4, 255, BRIGHT);
       }
       // We turn off the rest of the leds on that strip
       for(p = base + litPix + 1; p <= peak ; p++ ){
         leds[p]= CHSV( 0,0,0);
       }
-    } else{ // This should be the same as above, its used if frequecies_one is the higher value
-        litPix = Frequencies_One[i] / divisor;
-        for( p = base; p <= base + litPix; p ++ ) {
-          leds[p]= CHSV( Frequencies_One[i]/4, 255, 25);
+    } 
+    else{ // This should be the same as above, its used if frequecies_one is the higher value
+      litPix = Frequencies_One[i] / divisor;
+      for( p = base; p <= base + litPix; p ++ ) {
+        leds[p]= CHSV( Frequencies_One[i]/4, 255, BRIGHT);
+      }
+      for(p = base + litPix + 1; p <= peak ; p++ ){
+        leds[p]= CHSV( 0,0,0);
+      }
+    }
+  }
+}
+void Graph_Frequencies_Adjusted(){
+  for ( i = 0; i < NUM_STRPS; i++ ){
+    // We set the base and peak of the string of leds we want to work with
+    base = i * NUM_LEDS;
+    peak = (i * NUM_LEDS)-1 + NUM_LEDS;
+    // We use the higher of the two stereo frequency values
+    if(Frequencies_Two[i] > Frequencies_One[i]){
+      // The number of leds we want to be light up
+      litPix = Frequencies_Two[i] / divisor;
+      // We loop through and light up the leds for the strip we are on
+      // For the odd numbered strips we have to switch the order we light
+      // pixels so they all start from the bottom.
+      if( i % 2 != 0 ){
+        for( p = peak; p > peak - litPix; p -- ) {
+          leds[p]= CHSV( Frequencies_Two[i]/4, 255, BRIGHT);
         }
+        // We turn off the rest of the leds on that strip
+        for(p = base; p <= peak - litPix -1 ; p++ ){
+          leds[p]= CHSV( 0,0,0);
+        }
+      }
+      else {
+        for( p = base; p <= base + litPix; p ++ ) {
+          leds[p]= CHSV( Frequencies_Two[i]/4, 255, BRIGHT);
+        }
+        // We turn off the rest of the leds on that strip
         for(p = base + litPix + 1; p <= peak ; p++ ){
           leds[p]= CHSV( 0,0,0);
         }
+      }
+
+    } 
+    else{ // This should be the same as above, its used if frequecies_one is the higher value
+      litPix = Frequencies_One[i] / divisor;
+      if( i % 2 != 0 ){
+        for( p = peak; p > peak - litPix; p -- ) {
+          leds[p]= CHSV( Frequencies_One[i]/4, 255, BRIGHT);
+        }
+        // We turn off the rest of the leds on that strip
+        for(p = base; p <= peak - litPix -1 ; p++ ){
+          leds[p]= CHSV( 0,0,0);
+        }
+      }
+      else {
+        for( p = base; p <= base + litPix; p ++ ) {
+          leds[p]= CHSV( Frequencies_One[i]/4, 255, BRIGHT);
+        }
+        // We turn off the rest of the leds on that strip
+        for(p = base + litPix + 1; p <= peak ; p++ ){
+          leds[p]= CHSV( 0,0,0);
+        }
+      }
     }
   }
-}  
+}
