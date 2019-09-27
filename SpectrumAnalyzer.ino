08bf1375a43ccdded312f5b7bdfa661f9db9bfe8
Dustin Yoxall <dustin@cakeandeatitdesigns.com>
2019-09-27 15:08:43 -0400

    Updated code and added rotary encoder functionality

--- SpectrumAnalyzer.ino
+++ SpectrumAnalyzer.ino
@@ -3,7 +3,7 @@
 // NUM_STRPS more than 7 won't work since we only have 7 frequency channels
 #define NUM_STRPS 7
 #define DATA_PIN 9
-#define BRIGHT 45
+#define BRIGHT 65
 
 CRGB leds[NUM_LEDS * NUM_STRPS];
 
@@ -12,6 +12,10 @@ CRGB leds[NUM_LEDS * NUM_STRPS];
 #define RESET 6
 #define DC_One A0
 #define DC_Two A1
+// Rotary Encoder Pins
+#define ROTARY_A 13
+#define ROTARY_B 12
+#define ROATRY_BUTTON 11
 
 // Define spectrum variables
 int freq_amp;
@@ -24,11 +28,21 @@ int i;
 int divisor = 1024 / NUM_LEDS;
 int litPix;
 
+int Rot_Count = 0;
+int Rot_State;
+int Rot_Last_State;
+
 // Potentiometer Variables
 int potPin = 2;
 int potVal = 0;
 // This is used to store and modify the color value
 int adj_Val = 0;
+
+typedef void (*SimplePatternList[])();
+SimplePatternList patterns = {Graph_Frequencies_Middle,
+                              Graph_Frequencies_Adjusted, Graph_Frequencies};
+int total_paterns = (sizeof(patterns) / sizeof((patterns)[0]));
+uint8_t pattern_count = 0;
 
 void setup() {
   delay(2000);
@@ -37,6 +51,10 @@ void setup() {
   pinMode(RESET, OUTPUT);
   pinMode(DC_One, INPUT);
   pinMode(DC_Two, INPUT);
+  // Rotary Encoder
+  pinMode(ROTARY_A, INPUT);
+  pinMode(ROTARY_B, INPUT);
+
   digitalWrite(STROBE, HIGH);
   digitalWrite(RESET, HIGH);
 
@@ -54,15 +72,24 @@ void setup() {
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS * NUM_STRPS);
 
   Serial.begin(9600);
+
+  // Read initial state of Rotary Encoder
+  Rot_Last_State = digitalRead(ROTARY_A);
 }
 
 void loop() {
   Read_Pot();
   Read_Frequencies();
+  Read_Rotary();
   // Graph_Frequencies();
-  Graph_Frequencies_Adjusted();
-  FastLED.show();
-  delay(60);
+  // Graph_Frequencies_Adjusted();
+
+  EVERY_N_MILLISECONDS(
+      60) {  // FastLED based non-blocking delay to update/display the sequence.
+    patterns[pattern_count]();
+    // Graph_Frequencies_Middle();
+    FastLED.show();
+  }
 }
 
 void Read_Frequencies() {
@@ -75,32 +102,34 @@ void Read_Frequencies() {
   }
 }
 
+void Read_Rotary() {
+  Rot_State = digitalRead(ROTARY_A);
+  if (Rot_State != Rot_Last_State) {
+    if (digitalRead(ROTARY_B) != Rot_State) {
+      nextPattern();
+    } else {
+      prevPattern();
+    }
+    Serial.print("Position: ");
+    Serial.println(Rot_Count);
+  }
+  Rot_Last_State = Rot_State;
+}
+
 void Graph_Frequencies() {
   for (i = 0; i < NUM_STRPS; i++) {
     // We set the base and peak of the string of leds we want to work with
     base = i * NUM_LEDS;
     peak = (i * NUM_LEDS) - 1 + NUM_LEDS;
-    // We use the higher of the two stereo frequency values
-    if (Frequencies_Two[i] > Frequencies_One[i]) {
-      // The number of leds we want to be light up
-      litPix = Frequencies_Two[i] / divisor;
-      // We loop through and light up the leds for the strip we are on
-      for (p = base; p <= base + litPix; p++) {
-        leds[p] = CHSV(Frequencies_Two[i] / 4, 255, BRIGHT);
-      }
-      // We turn off the rest of the leds on that strip
-      for (p = base + litPix + 1; p <= peak; p++) {
-        leds[p] = CHSV(0, 0, 0);
-      }
-    } else {  // This should be the same as above, its used if frequecies_one is
-              // the higher value
-      litPix = Frequencies_One[i] / divisor;
-      for (p = base; p <= base + litPix; p++) {
-        leds[p] = CHSV(Frequencies_One[i] / 4, 255, BRIGHT);
-      }
-      for (p = base + litPix + 1; p <= peak; p++) {
-        leds[p] = CHSV(0, 0, 0);
-      }
+    int freq_val = (Frequencies_Two[i] + Frequencies_One[i]) / 2;
+    litPix = freq_val / divisor;
+    // We loop through and light up the leds for the strip we are on
+    for (p = base; p <= base + litPix; p++) {
+      leds[p] = CHSV(freq_val / 4, 255, BRIGHT);
+    }
+    // We turn off the rest of the leds on that strip
+    for (p = base + litPix + 1; p <= peak; p++) {
+      leds[p] = CHSV(0, 0, 0);
     }
   }
 }
@@ -140,15 +169,11 @@ void Graph_Frequencies_Adjusted() {
   }
 }
 void Graph_Frequencies_Middle() {
-  int pot_val = 0;
-  int freq_val = 0;
-  int edge = 0;
   // We bring the value into a 256 bit range to pass to the fastLED library.
-  pot_val = Read_Pot() / 4;
-
+  int pot_val = Read_Pot() / 4;
   for (i = 0; i < NUM_STRPS; i++) {
     // We average the frequencies to get our value.
-    freq_val = (Frequencies_Two[i] + Frequencies_One[i]) / 2;
+    int freq_val = (Frequencies_Two[i] + Frequencies_One[i]) / 2;
     // We translate the frequency into the number of LEDs we want to light up.
     litPix = freq_val / divisor;
     // We need an even number to display properly
@@ -157,11 +182,11 @@ void Graph_Frequencies_Middle() {
     }
     // We find the number of LEDs on the edges. We divide by two to make them
     // evenly spaced in the middle of the strip. (these will be turned off);
-    edge = (NUM_LEDS - litPix) / 2;
+    int edge = (NUM_LEDS - litPix) / 2;
     // We set the range for the LED strip we are working with.
     base = i * NUM_LEDS;
     peak = (i * NUM_LEDS) - 1 + NUM_LEDS;
-    for (p = base; p <= base; p++) {
+    for (p = base; p <= peak; p++) {
       // If p is in the edges turn the leds off, otherwise we turn them on using
       // the pot_val.
       if (p <= (base + edge) || p >= (peak - edge)) {
@@ -183,4 +208,15 @@ int Adjust_Color_Value(int val, int freq) {
     store = store - 255;
   }
   return store;
+}
+
+void nextPattern() {
+  // add one to the current pattern number, and wrap around at the end
+  pattern_count = (pattern_count + 1) % total_paterns;
+}
+void prevPattern() {
+  pattern_count = (pattern_count - 1);
+  if (pattern_count > total_paterns) {
+    pattern_count = total_paterns - 1;
+  }
 }
